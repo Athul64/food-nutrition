bfe0a2645a88db3ccb941c8c2a127f4df6c9699c
Athul64 <athulsylesh23@gmail.com>
2024-09-10 00:14:37 +0530

    path

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const port = process.env.PORT || 3200;
 const app = express();
 app.use(express.json()); // For parsing application/json
 app.use(cors()); // Enable CORS for all origins
 
+const MONGO_URI='mongodb+srv://athul:mongodb%40athul@cluster0.rupdk.mongodb.net/food-nutrition?retryWrites=true&w=majority&appName=Cluster0';
+
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -116,7 +117,6 @@ app.delete('/api/foods/:id', async (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
